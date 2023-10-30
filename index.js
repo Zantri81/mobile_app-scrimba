f8125959c11745e4f7115bcc72d3dd0cb984970b
Zantri81 <tristanzanetti@yahoo.fr>
2023-10-30 18:49:08 +0100

    changement et ajout d'import dans le JS (Firebase)

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
-let cart = []
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"       //importer ref, push quand il y a besoin
+
+const appSettings = {
+    databaseURL: "https://realtime-database-c25fc-default-rtdb.europe-west1.firebasedatabase.app/"      //ajouter la database de firebase
+}
+
+const app = initializeApp(appSettings)      //connecte le projet avec Firebase
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
 
 const inputFieldEL = document.getElementById("input-field")
 const addBtnEL = document.getElementById("add-button")
 
 addBtnEL.addEventListener("click", function() {
-    cart.push(inputFieldEL.value)
-    inputFieldEL.value =""
-    console.log(cart)
-
+    let inputValue = inputFieldEL.value
+    
+    // Challenge: Use the Firebase function 'push' to push inputValue to the database
+    inputValue.push(shoppingListInDB, inputValue)
+    
+    console.log(inputValue)
 })
