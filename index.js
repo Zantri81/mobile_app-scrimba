b8d31387276868038d8ab1da03779d5ba50f8744
Zantri81 <tristanzanetti@yahoo.fr>
2023-10-30 15:45:27 +0100

    première fonction et addEventListener

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
+let cart = []
+
+const inputFieldEL = document.getElementById("input-field")
+const addBtnEL = document.getElementById("add-button")
+
+addBtnEL.addEventListener("click", function() {
+    cart.push(inputFieldEL.value)
+    inputFieldEL.value =""
+    console.log(cart)
+
+})
